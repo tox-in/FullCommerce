ce5fbaa0353d214e5c1b9a2ef00a97becb1852c0
tox-in <habimanat78@gmail.com>
2025-05-02 13:45:01 +0200

    documentation base

---
+++ backend/src/utils/swagger.js
@@ -0,0 +1,33 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'E-commerce API',
+            version: '1.0.0',
+            description: 'API documentation for the E-commerce platform',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/api',
+                description: 'Development server',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+    },
+    apis: ['./src/routes/*.js', './src/controllers/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
